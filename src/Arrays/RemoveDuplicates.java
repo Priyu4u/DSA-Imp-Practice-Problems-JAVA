50cfb7efc19040a0123ea1bb964919ce5b54b02b
Priyanshu Gupta <gpriyanshuu4u@gmail.com>
2024-11-01 19:56:20 +0530

    remove duplicates

---
+++ src/Arrays/RemoveDuplicates.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+// Given an integer array nums sorted in non-decreasing order,
+// remove the duplicates in-place such that each unique element
+// appears only once. The relative order of the elements should
+// be kept the same. Then return the number of unique elements
+// in nums
+public class RemoveDuplicates {
+
+    static int remove(int[] nums){
+        int n = nums.length;
+        for(int i=0; i<n; i++)
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,2,2,2,3,3,4,5,5};
+
+    }
+}
